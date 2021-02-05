fe11d4ffc674241097009cbeb45bb352fd7dd8a6
liyang <liyang368@126.com>
2021-02-05 17:44:15 +0800

    add data

---
+++ static/data.js
@@ -0,0 +1,205 @@
+var citys = [
+  {
+    from: "北京",
+    to: "石家庄",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "石家庄",
+    to: "北京",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "天津",
+    to: "上海",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "上海",
+    to: "天津",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "重庆",
+    to: "石家庄",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "石家庄",
+    to: "重庆",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "太原",
+    to: "石家庄",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "石家庄",
+    to: "太原",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "呼和浩特",
+    to: "石家庄",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "石家庄",
+    to: "呼和浩特",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "哈尔滨",
+    to: "石家庄",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "石家庄",
+    to: "哈尔滨",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "沈阳",
+    to: "石家庄",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "石家庄",
+    to: "沈阳",
+    count: 10000 * Math.random(),
+  },
+
+  {
+    from: "哈尔滨",
+    to: "杭州",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "杭州",
+    to: "哈尔滨",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "沈阳",
+    to: "杭州",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "杭州",
+    to: "沈阳",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "福州",
+    to: "杭州",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "杭州",
+    to: "福州",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "哈尔滨",
+    to: "武汉",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "武汉",
+    to: "哈尔滨",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "沈阳",
+    to: "武汉",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "武汉",
+    to: "沈阳",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "福州",
+    to: "武汉",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "武汉",
+    to: "福州",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "石家庄",
+    to: "武汉",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "武汉",
+    to: "石家庄",
+    count: 10000 * Math.random(),
+  },
+
+  {
+    from: "哈尔滨",
+    to: "襄阳",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "襄阳",
+    to: "哈尔滨",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "沈阳",
+    to: "襄阳",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "襄阳",
+    to: "沈阳",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "福州",
+    to: "襄阳",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "襄阳",
+    to: "福州",
+    count: 10000 * Math.random(),
+  },
+
+  {
+    from: "重庆",
+    to: "广州",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "广州",
+    to: "重庆",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "武汉",
+    to: "广州",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "广州",
+    to: "武汉",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "福州",
+    to: "广州",
+    count: 10000 * Math.random(),
+  },
+  {
+    from: "广州",
+    to: "福州",
+    count: 10000 * Math.random(),
+  }
+]
